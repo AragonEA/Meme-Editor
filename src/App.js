b233d936f1443f49e523433b91eb808e80de858f
Aragon Emanuel <aragon3m4@gmail.com>
2023-10-15 08:48:09 -0300

    Add icons to title

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Selector from './components/Selector';
 function App() {
   return (
     <div className="App nes-container with-title is-centered">
-      <h1 className="title">Meme Editor</h1>
+      <h1 className="title"><i class="nes-icon is-small star"></i> Meme Editor  <i class="nes-icon is-small star"></i></h1>
       <p>Select an image to add your text to it</p>
       <Editor />
       <Selector />
